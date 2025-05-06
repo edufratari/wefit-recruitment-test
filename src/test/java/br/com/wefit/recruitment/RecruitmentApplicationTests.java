ec8d04e4df2509f6bd56e047bb1ab41fba40d3d6
edufratari <edufratari@hotmail.com>
2025-05-06 14:09:05 -0300

    add files

---
+++ src/test/java/br/com/wefit/recruitment/RecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wefit.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
